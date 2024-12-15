ae5342a4b6735db7d7979bd67912ae1092ae1ee3
nika todua <alchimikos@gmail.com>
2024-12-15 23:54:31 +0400

    push

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: false,
+  
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+export class CarouselComponent {
+
+  array = ["https://wander-lush.org/wp-content/uploads/2020/12/Ultimate-Georgia-itinerary-Khulo-cable-car.jpg",
+    "https://www.goingthewholehogg.com/wp-content/uploads/Georgia-Page-Header-M.jpg",
+    "https://storage.georgia.travel/images/martvili-canyon-gnta-m.webp",
+    "https://media-cdn.tripadvisor.com/media/photo-s/15/8d/48/f4/okace.jpg",
+    "https://cdn.georgiantravelguide.com/storage/files/chanchkeri-1.jpg",
+    "https://cf.bstatic.com/xdata/images/hotel/max1024x768/459182848.jpg?k=7faa25bf46618aefd00d75baedd3f602a021a30afb66599fc02901852d063c5f&o=&hp=1",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa700zhuR_FzwqsX5D728KKsXbBnMlhLgxpw&s",
+    "https://scontent.ftbs6-2.fna.fbcdn.net/v/t1.6435-9/78495081_2393567320908932_8531825063169622016_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=E-_PKJ3iqxcQ7kNvgHbCBdG&_nc_zt=23&_nc_ht=scontent.ftbs6-2.fna&_nc_gid=Aml451JsQJ035BGbBP9d9dG&oh=00_AYBmeG6hCM7dR5fWylzeg4PBY-xPaUU9D-rQO_30lpxFxw&oe=67869AAE"
+  ];
+
+
+  moveNext(){
+    let items = document.querySelectorAll(".items")
+    let boxcont = document.querySelector(".cont")
+
+    boxcont?.appendChild(items[0])
+  }
+  movePrev(){
+    let items = document.querySelectorAll(".items")
+    let boxcont = document.querySelector(".cont")
+
+    boxcont?.prepend(items[items.length - 1])
+  }
+  
+  
+
+  ngOnInit(){
+    
+    document.addEventListener("DOMContentLoaded", () => {
+      let box = document.querySelector(".box")
+
+     
+      
+    })
+    
+    
+    
+  }
+
+}
+interface OnInit {
+  ngOnInit(): void;
+}
