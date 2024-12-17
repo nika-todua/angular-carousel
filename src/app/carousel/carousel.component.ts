203602dd9969b2a01a20df29fdcbdd11a7d5559c
nika todua <alchimikos@gmail.com>
2024-12-17 22:28:00 +0400

    update

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -3,7 +3,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   standalone: false,
-  
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
@@ -13,24 +12,32 @@ export class CarouselComponent {
 
   array:any = [];
 
+  // card transition code
+  transitionTime:string = "220ms";
+  cubicbezier:string = 'cubic-bezier(0.79, 0.06, 0.37, 0.89)'
+  transitioncode:string = `transition: all ${this.transitionTime} ${this.cubicbezier} !important; -o-transition: all ${this.transitionTime} ${this.cubicbezier} !important; -ms-transition: all ${this.transitionTime} ${this.cubicbezier} !important; -moz-transition: all ${this.transitionTime} ${this.cubicbezier} !important; -webkit-transition: all ${this.transitionTime} ${this.cubicbezier} !important;`
+
   moveNext(){
     let items = document.querySelectorAll(`.item-${this.carouselId[0]}`)
     let boxcont = document.querySelector(`.cont-${this.carouselId[0]}`)
 
-    boxcont?.appendChild(items[0])
+    boxcont!.appendChild(items[0])
   }
   movePrev(){
     let items = document.querySelectorAll(`.item-${this.carouselId[0]}`)
     let boxcont = document.querySelector(`.cont-${this.carouselId[0]}`)
 
-    boxcont?.prepend(items[items.length - 1])
+    boxcont!.prepend(items[items.length - 1])
   }
-  
-  
-
   ngOnInit(){
     this.itemId.emit(this.carouselId);
     this.array = this.carouselId[1]
+
+    document.addEventListener("DOMContentLoaded", () => {
+      let items:any = document.querySelectorAll(`.item-${this.carouselId[0]}`)
+      items.forEach((item: any) => item.setAttribute("style", this.transitioncode) )
+    })
+    
   }
 
 }
